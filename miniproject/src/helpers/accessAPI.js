3dfc2c496c1ead598adb643e4668c70f20fa5433
safarudinyoga <safarudinyoga@gmail.com>
2019-12-04 17:24:53 +0700

    changes

--- miniproject/src/helpers/accessAPI.js
+++ miniproject/src/helpers/accessAPI.js
@@ -1,6 +1,4 @@
 import axios from "axios";
 import { API_URL } from "../config";
 
-// API_URL += '/3/movie/550?api_key=9ba639c7c8d42e4b827058a99fb728e5'
-
 export const request = axios.create({ baseURL: API_URL, timeout: 3000 });
