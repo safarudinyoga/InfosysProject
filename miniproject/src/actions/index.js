5e932dcf1e1363f30c16d5f8533da8adf7cdc6c6
safarudinyoga <safarudinyoga@gmail.com>
2019-12-04 17:24:16 +0700

    adding action for movie and detail

--- miniproject/src/actions/index.js
+++ miniproject/src/actions/index.js
@@ -2,9 +2,12 @@ import {
     LOAD_DATA_SUCCESS,
     LOAD_DATA_FAILURE,
     LOAD_IMAGE_SUCCESS,
-    LOAD_IMAGE_FAILURE
+    LOAD_IMAGE_FAILURE,
+    LOAD_DETAIL_SUCCESS,
+    LOAD_DETAIL_FAILURE
 } from '../constants/constant';
 import { request } from '../helpers/accessAPI';
+import { push } from "connected-react-router";
 
 
 const loadDataSuccess = (movie) => {
@@ -21,12 +24,37 @@ const loadDataFailure = () => {
 
 export const loadData = () => {
     return dispatch => {
-        return request.get(`/3/movie/550?api_key=9ba639c7c8d42e4b827058a99fb728e5`).then(response => {
-            // console.log(response.data);
-            dispatch(loadDataSuccess(response.data))
+        return request.get(`/3/discover/movie?api_key=9ba639c7c8d42e4b827058a99fb728e5&language=en-US&sort_by=popularity.desc`).then(response => {
+            // console.log(response.data.results)
+            dispatch(loadDataSuccess(response.data.results))
         }).catch(err => {
             console.log(err);
             dispatch(loadDataFailure())
+        })
+    }
+}
+
+const loadDetailSuccess = detail => {
+    return {
+        type: LOAD_DETAIL_SUCCESS, detail
+    }
+}
+
+const loadDetailFailure = () => {
+    return {
+        type: LOAD_DETAIL_FAILURE
+    }
+}
+
+export const loadDetail = (id) => {
+    return dispatch => {
+        return request.get(`/3/movie/${id}?api_key=9ba639c7c8d42e4b827058a99fb728e5&language=en-US`).then(response => {
+            // console.log('response', response);
+            dispatch(loadDetailSuccess(response.data))
+            dispatch(push(`detail/`))
+        }).catch(err => {
+            console.log(err)
+            dispatch(loadDetailFailure())
         })
     }
 }
@@ -46,7 +74,7 @@ const loadImageFailure = () => {
 export const loadImage = () => {
     return dispatch => {
         return request.get(`/3/configuration?api_key=9ba639c7c8d42e4b827058a99fb728e5`).then(response => {
-            // console.log('index-image',response.data.images);
+            // console.log('index-image', response.data.images);
             dispatch(loadImageSuccess(response.data.images))
         }).catch(err => {
             console.log(err);
