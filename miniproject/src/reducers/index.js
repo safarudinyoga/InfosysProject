c7ffa3136a8255b5308963b03e0f8773c16f5a89
safarudinyoga <safarudinyoga@gmail.com>
2019-12-04 17:22:54 +0700

    index reducers

--- miniproject/src/reducers/index.js
+++ miniproject/src/reducers/index.js
@@ -2,9 +2,10 @@ import { combineReducers } from "redux";
 import { connectRouter } from "connected-react-router";
 import movie from './movie';
 import image from './image';
+import detail from './detail'
 
 export default history =>
     combineReducers({
-        router: connectRouter(history), movie, image //,
+        router: connectRouter(history), movie, image, detail //,
     });
 
