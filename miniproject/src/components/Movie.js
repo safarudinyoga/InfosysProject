149e712d66731d62ceca0b1fcdcf10e58a947bc2
safarudinyoga <safarudinyoga@gmail.com>
2019-12-04 17:17:23 +0700

    component box luar

--- miniproject/src/components/Movie.js
+++ miniproject/src/components/Movie.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import BoxFilm from './ListItem/BoxFilm';
 import Header from './Header';
+import { CardDeck } from 'react-bootstrap';
 
 export default class Movie extends Component {
     render() {
