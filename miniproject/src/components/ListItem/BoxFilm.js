d3e0b621b0beb0a85673c66c21384864e5b0f8bd
safarudinyoga <safarudinyoga@gmail.com>
2019-12-04 17:20:31 +0700

    get loadData API

--- miniproject/src/components/ListItem/BoxFilm.js
+++ miniproject/src/components/ListItem/BoxFilm.js
@@ -1,21 +1,55 @@
 import React, { Component } from 'react'
 import ItemCard from './ItemCard'
 import { CardDeck } from 'react-bootstrap'
+import { connect } from 'react-redux';
+import { loadData } from '../../actions/index';
 
-export default class BoxFilm extends Component {
+class BoxFilm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: '',
+            price: '',
+            original_title: '',
+            vote_average: '',
+            vote_count: '',
+            poster_path: '',
+            jam: '',
+            menit: ''
+        };
+    }
+
+    componentDidMount() {
+        this.props.loadData()
+    }
+
     render() {
+        // console.log('data box', this.props.data);
         return (
             <div className="container-fluid ">
                 <div className="card card-responsive-width" style={{ padding: '5vh' }}>
                     <CardDeck>
-                        {/* {console.log(this.props)} */}
-                        {/* {this.props.products.map((value, index) => {
-                            return <ItemCard key={index} product={value} />
-                        })} */}
-                        <ItemCard />
+                        {Object.values(this.props.data).map((item, index) => {
+                            return (
+                                <ItemCard key={index} data={item} />
+                            )
+                        })}
                     </CardDeck>
                 </div>
             </div>
         )
     }
 }
+
+const mapStateToProps = (state) => ({
+    data: state.movie
+})
+
+const mapDispatchToProps = (dispatch, ownProps) => ({
+    loadData: () => dispatch(loadData())
+})
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(BoxFilm)
